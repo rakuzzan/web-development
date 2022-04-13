bb40a1a4ef278f080eee2ade4f31eb6e7884513f
rakuzzan <marishka1581@gmail.com>
2022-04-13 20:04:08 +0300

    lw9 update

--- lw9/PrimeNumber.js
+++ lw9/PrimeNumber.js
@@ -1,21 +1,28 @@
 function isPrime(num)
 {
     if (Array.isArray(num))
-    {
-        for(let i = 0; i < Array.length; i++)
-        {
-            for(let k = 2, s = Math.sqrt(num[i]); k <= s; k++)
-                if(num[i] % k === 0)
-                    return console.log(num[i] + " isn't Prime number");
-            console.log(num[i] + " is Prime number") ;
-        }
-    }
-    else
+        for(let i = 0; i < num.length; i++)
+            if (num[i] > 2)
+                for(let k = 2, s = Math.sqrt(num[i]); k <= s; k++)
+                    if(num[i] % k === 0)
+                    {
+                        console.log(num[i] + " isn't Prime number")
+                        break
+                    }
+                    else
+                    {
+                        console.log(num[i] + " is Prime number")
+                        break
+                    }
+            else
+                console.log(num[i] + " is Prime number")
+    else if (Number.isInteger(num))
     {
         for(let j = 2, s = Math.sqrt(num); j <= s; j++)
             if(num % j === 0)
-                return console.log(num + " isn't Prime number");
-        console.log(num + " is Prime number") ;
+                return console.log(num + " isn't Prime number")
+        console.log(num + " is Prime number")
     }
-
+    else
+        console.log("Wrong input")
 }
